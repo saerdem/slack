d448c68ffa505c482df6fe53e2a5e358ae416850
saerdem <saerdem@gmail.com>
2020-05-12 19:24:18 +0300

    add Login.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,5 +3,6 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User able to create CHANNELS");
+        //changes here
     }
 }
---
+++ src/day_1/Login.java
@@ -0,0 +1,7 @@
+package day_1;
+
+public class Login {
+    public static void main(String[] args) {
+        //login code here..
+    }
+}
