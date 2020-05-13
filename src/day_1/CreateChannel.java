e8a7329564d88f99e51f797fe918052de853f48e
saerdem <saerdem@gmail.com>
2020-05-13 18:08:41 +0300

    update CreateChannel.java -conflict-

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,7 +4,7 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User able to create CHANNELS");
 
-        //additional code from INTELLIJ .
+        //changes from INTELLIJ .
         int x=0;
         if (x==0){
             System.out.println("equal");
