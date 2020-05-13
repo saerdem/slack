54c89ed374085e5f31f1d0019c5f1f69a6dcb3ac
saerdem <61096749+saerdem@users.noreply.github.com>
2020-05-13 17:59:20 +0300

    Update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -4,5 +4,13 @@ public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User able to create CHANNELS");
         //changes here
+
+        //changes from GITHUB
+        String s= "a";
+        if(s.contains("a")){
+            System.out.println("contains");
+        }else{
+            System.out.println("not");
+        }
     }
 }
