f38cbbcf4bf7b8a9dce39fd0416e5424928659bc
saerdem <saerdem@gmail.com>
2020-05-13 17:57:15 +0300

    update CreateChannel.java

--- src/day_1/CreateChannel.java
+++ src/day_1/CreateChannel.java
@@ -3,7 +3,13 @@ package day_1;
 public class CreateChannel {
     public static void main(String[] args) {
         System.out.println("User able to create CHANNELS");
-        //additional code here
+
+        //additional code from INTELLIJ .
+        int x=0;
+        if (x==0){
+            System.out.println("equal");
+        }
+
 
     }
 }
